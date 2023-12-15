213ebf24dcb428e945f2886f3b688dd05ed25527
Your Name <you@example.com>
2023-12-15 13:33:34 -0800

    up

---
+++ sort.h
@@ -0,0 +1,34 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer
+ * @prev: Pointer
+ * @next: Pointer
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+void shell_sort(int *array, size_t size);
+
+#endif
